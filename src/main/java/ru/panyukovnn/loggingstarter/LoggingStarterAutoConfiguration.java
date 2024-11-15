0438ddb2827c8175d67310fffd2dfe430e31b8fb
Nikolay Panyukov <panyukovnn@gmail.com>
2024-11-15 14:49:21 +0300

    Деплой версии 1.3-SNAPSHOT

--- src/main/java/ru/panyukovnn/loggingstarter/LoggingStarterAutoConfiguration.java
+++ src/main/java/ru/panyukovnn/loggingstarter/LoggingStarterAutoConfiguration.java
@@ -1,8 +1,33 @@
 package ru.panyukovnn.loggingstarter;
 
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import ru.panyukovnn.loggingstarter.aspect.LogExecutionAspect;
+import ru.panyukovnn.loggingstarter.webfilter.WebLoggingFilter;
+import ru.panyukovnn.loggingstarter.webfilter.WebLoggingRequestBodyAdvice;
+
+@AutoConfiguration
+@ConditionalOnProperty(prefix = "logging", value = "enabled", havingValue = "true", matchIfMissing = true)
 public class LoggingStarterAutoConfiguration {
 
-    public static void println(String str) {
-        System.out.println("Вызвано из gradle библиотеки: " + str);
+    @Bean
+    @ConditionalOnProperty(prefix = "logging", value = "log-exec-time", havingValue = "true")
+    public LogExecutionAspect logExecutionAspect() {
+        return new LogExecutionAspect();
+    }
+
+    @Bean
+    @ConditionalOnProperty(prefix = "logging.web-logging", value = "enabled", havingValue = "true", matchIfMissing = true)
+    public WebLoggingFilter webLoggingFilter() {
+        return new WebLoggingFilter();
+    }
+
+    @Bean
+    @ConditionalOnBean(WebLoggingFilter.class)
+    @ConditionalOnProperty(prefix = "logging.web-logging", value = "log-body", havingValue = "true")
+    public WebLoggingRequestBodyAdvice webLoggingRequestBodyAdvice() {
+        return new WebLoggingRequestBodyAdvice();
     }
 }
