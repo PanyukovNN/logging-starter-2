1aa44ecde983638672d1f2607a7858b8adfb8c52
Nikolay Panyukov <panyukovnn@gmail.com>
2024-12-28 09:39:35 +0300

    Добавлено логгирование feign

--- src/main/java/ru/panyukovnn/loggingstarter/LoggingStarterAutoConfiguration.java
+++ src/main/java/ru/panyukovnn/loggingstarter/LoggingStarterAutoConfiguration.java
@@ -1,10 +1,13 @@
 package ru.panyukovnn.loggingstarter;
 
+import feign.Logger;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import ru.panyukovnn.loggingstarter.aspect.LogExecutionAspect;
+import ru.panyukovnn.loggingstarter.feign.FeignRequestLogger;
+import ru.panyukovnn.loggingstarter.service.LoggingService;
 import ru.panyukovnn.loggingstarter.webfilter.WebLoggingFilter;
 import ru.panyukovnn.loggingstarter.webfilter.WebLoggingRequestBodyAdvice;
 
@@ -29,5 +32,22 @@ public class LoggingStarterAutoConfiguration {
     @ConditionalOnProperty(prefix = "logging.web-logging", value = "log-body", havingValue = "true")
     public WebLoggingRequestBodyAdvice webLoggingRequestBodyAdvice() {
         return new WebLoggingRequestBodyAdvice();
+    }
+
+    @Bean
+    public LoggingService loggingService() {
+        return new LoggingService();
+    }
+
+    @Bean
+    @ConditionalOnProperty(prefix = "logging.web-logging", value = "log-feign-requests", havingValue = "true")
+    public FeignRequestLogger feignRequestLogger() {
+        return new FeignRequestLogger();
+    }
+
+    @Bean
+    @ConditionalOnProperty(prefix = "logging.web-logging", value = "log-feign-requests", havingValue = "true")
+    public Logger.Level feignLoggerLevel() {
+        return Logger.Level.BASIC;
     }
 }
--- src/main/java/ru/panyukovnn/loggingstarter/webfilter/WebLoggingRequestBodyAdvice.java
+++ src/main/java/ru/panyukovnn/loggingstarter/webfilter/WebLoggingRequestBodyAdvice.java
@@ -1,33 +1,27 @@
 package ru.panyukovnn.loggingstarter.webfilter;
 
 import jakarta.servlet.http.HttpServletRequest;
-import org.apache.logging.log4j.util.Strings;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.MethodParameter;
 import org.springframework.http.HttpInputMessage;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.RequestBodyAdviceAdapter;
+import ru.panyukovnn.loggingstarter.service.LoggingService;
 
 import java.lang.reflect.Type;
-import java.util.Optional;
 
 @ControllerAdvice
 public class WebLoggingRequestBodyAdvice extends RequestBodyAdviceAdapter {
 
-    private static final Logger log = LoggerFactory.getLogger(WebLoggingRequestBodyAdvice.class);
-
     @Autowired
     private HttpServletRequest request;
+    @Autowired
+    private LoggingService loggingService;
 
     @Override
     public Object afterBodyRead(Object body, HttpInputMessage inputMessage, MethodParameter parameter, Type targetType, Class<? extends HttpMessageConverter<?>> converterType) {
-        String method = request.getMethod();
-        String requestURI = request.getRequestURI() + formatQueryString(request);
-
-        log.info("Тело запроса: {} {} {}", method, requestURI, body);
+        loggingService.logRequestBody(request, body);
 
         return super.afterBodyRead(body, inputMessage, parameter, targetType, converterType);
     }
@@ -35,12 +29,6 @@ public class WebLoggingRequestBodyAdvice extends RequestBodyAdviceAdapter {
     @Override
     public boolean supports(MethodParameter methodParameter, Type targetType, Class<? extends HttpMessageConverter<?>> converterType) {
         return true;
-    }
-
-    private String formatQueryString(HttpServletRequest request) {
-        return Optional.ofNullable(request.getQueryString())
-            .map(qs -> "?" + qs)
-            .orElse(Strings.EMPTY);
     }
 }
 
